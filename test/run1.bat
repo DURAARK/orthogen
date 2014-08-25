c0deb762ccfaa0d6c5de5067e79b567170679258
Ulrich Krispel <uli@krispel.net>
2014-08-25 14:21:32 +0200

    add doc and test commandline scripts

---
+++ test/run1.bat
@@ -0,0 +1,2 @@
+..\..\MSVC2013\Release\orthogen.exe --im=..\00_hdrpanorama2.pnm --ig=..\ransacplanes.obj
+pause
---
+++ test/run2.bat
@@ -0,0 +1,2 @@
+..\..\MSVC2013\Release\orthogen.exe --im=..\Scan00_Hdr.ppm --ig=..\ransacplanes.obj --rot 0.003361 0.002216 0.99964 0.026682 --res 1.0 --elmin=-1.046 --elmax=1.5694
+pause
