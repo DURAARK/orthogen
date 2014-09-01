8db48268bfc140f3d6b24e1955d202d74a09b24f
uli <uli@krispel.net>
2014-09-01 23:52:57 +0200

    simple mean shift

--- src/meanshift.h
+++ src/meanshift.h
@@ -9,6 +9,9 @@ typedef Eigen::Vector3d Vec3d;
 
 class Meanshift
 {
+private:
+	std::vector < Vec3d > points;
+	std::vector < Vec3d > window;
 
 public:
 	template< typename VTYPE >
@@ -25,35 +28,57 @@ public:
 		}
 	};
 
-	std::vector < std::pair < Vec3d, int > > points;
-	std::set < Vec3d, EigenLexicographicalComparator< Vec3d > > clustercenters;
 
-	// give minimum and maximum, as well as number of clusters per dimension
-	// (will perform regular sampling)
-	void calculate(const Vec3d &pmin, const Vec3d &pmax, const double clusterwidth)
+	// perform meanshift, rectangular window, one window per point
+	// very simple and slow
+	void calculate(const Vec3d &pmin, const Vec3d &pmax, const double windowwidth)
 	{
-		const Vec3d clusters = (pmax - pmin) / clusterwidth;
-		const int clustersx = (int)ceil(clusters[0]);
-		const int clustersy = (int)ceil(clusters[1]);
-		const int clustersz = (int)ceil(clusters[2]);
-		const double cwx = clusters[0] - floor(clusters[0]);
-		const double cwy = clusters[1] - floor(clusters[1]);
-		const double cwz = clusters[2] - floor(clusters[2]);
-		for (int iz = 0; iz < clustersz; ++iz)
+		auto const inslab = [windowwidth](const double val, const double center) -> bool
 		{
-			for (int iy = 0; iy < clustersy; ++iy)
-			{
-				for (int ix = 0; ix < clustersx; ++ix)
-				{
-					// create a cluster
-					clustercenters.insert(
-						Vec3d( 
-							pmin[0] + (cwx/2.0) + ix*clusterwidth,
-							pmin[1] + (cwy/2.0) + iy*clusterwidth,
-							pmin[2] + (cwz/2.0) + iz*clusterwidth
-						) );
+			return (val < (center - windowwidth)) || (val >(center + windowwidth));
+		};
 
+		std::vector<bool> converged;
+		converged.resize(points.size(), false);
+
+		window = points;
+
+		bool allconverged = false;
+		
+		while (!allconverged)
+		{
+			allconverged = true;
+			int i = 0;
+			for (auto &c : window)
+			{
+				if (!converged[i])
+				{
+					std::vector<Vec3d> inpoints;
+					// filter points inside window
+					for (auto const &p : points)
+					{
+						if (inslab(p[0], c[0]) && inslab(p[1], c[1]) && inslab(p[2], c[2]))
+						{
+							inpoints.push_back(p);
+						}
+					}
+					// calculate mean shift
+					Vec3d mean = Vec3d(0.0, 0.0, 0.0);
+					for (auto const &p : points) mean += p;
+					mean /= (double)points.size();
+
+					Vec3d ms = mean - c;
+					if (sqrt(ms[0] * ms[0] + ms[1] * ms[1] + ms[2] * ms[2]) < 0.01)
+					{
+						converged[i] = true;
+					}
+					else 
+					{
+						allconverged = false;
+						c += mean - c;
+					}
 				}
+				++i; // window index
 			}
 		}
 	}
