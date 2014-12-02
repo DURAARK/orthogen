10017db92bf66ef912ae7cef0513ed17478956e1
Ulrich Krispel <uli@krispel.net>
2014-12-02 13:00:56 +0100

    refactor dimension of meanshift vector to template parameter

--- src/meanshift.h
+++ src/meanshift.h
@@ -1,5 +1,6 @@
 /*
  * Simple Meanshift implementation
+ * 
  */
 
 #include <set>
@@ -8,88 +9,93 @@
 #include "types.h"
 
 namespace MEANSHIFT
-
 {
 
-    typedef Eigen::Vector3d Vec3d;
+template < class VEC, int D > 
+class Meanshift
+{
+private:
+    std::vector < VEC > points;
+    std::vector < VEC > window;
+    
+    const double shift_threshold; // window is considered stable if 
+                                   // shift is lesser than this threshold
 
-    class Meanshift
+public:
+
+    Meanshift(const double sh_th=0.1) : shift_threshold(sh_th)
     {
-    private:
-        std::vector < Vec3d > points;
-        std::vector < Vec3d > window;
+    }
 
-    public:
-        template< typename VTYPE >
-        struct EigenLexicographicalComparator
+    // perform very simple and slow meanshift:
+    // rectangular window, one window per point
+    void calculate(const VEC3 &pmin, const VEC3 &pmax, const double windowwidth)
+    {
+        auto const inslab = [windowwidth](const double val, const double center) -> bool
         {
-            inline bool operator() (const VTYPE &lhs, const VTYPE &rhs) const
-            {
-                for (int i = 0; i < VTYPE::RowsAtCompileTime; ++i)
-                {
-                    if (lhs[i] < rhs[i]) return true;
-                    if (lhs[i] > rhs[i]) return false;
-                }
-                return false;
-            }
+            return (val < (center - windowwidth)) || (val >(center + windowwidth));
         };
 
+        std::vector<bool> converged;
+        converged.resize(points.size(), false);
 
-        // perform meanshift, rectangular window, one window per point
-        // very simple and slow
-        void calculate(const Vec3d &pmin, const Vec3d &pmax, const double windowwidth)
+        window = points;
+
+        bool allconverged = false;
+
+        while (!allconverged)
         {
-            auto const inslab = [windowwidth](const double val, const double center) -> bool
+            allconverged = true;
+            int i = 0;
+            for (auto &c : window)
             {
-                return (val < (center - windowwidth)) || (val >(center + windowwidth));
-            };
-
-            std::vector<bool> converged;
-            converged.resize(points.size(), false);
-
-            window = points;
-
-            bool allconverged = false;
-
-            while (!allconverged)
-            {
-                allconverged = true;
-                int i = 0;
-                for (auto &c : window)
+                // only consider windows that have not converged yet
+                if (!converged[i])
                 {
-                    if (!converged[i])
+                    std::vector<VEC3> inpoints;
+                    // filter points inside window
+                    for (auto const &p : points)
                     {
-                        std::vector<Vec3d> inpoints;
-                        // filter points inside window
-                        for (auto const &p : points)
-                        {
-                            if (inslab(p[0], c[0]) && inslab(p[1], c[1]) && inslab(p[2], c[2]))
-                            {
-                                inpoints.push_back(p);
-                            }
+                      bool isInside=true;
+                      for (int d=0;d<D;++d) 
+                      {
+                        if (!inslab(p[d],c[d]) 
+                        { 
+                          isInside=false;
+                          break;
                         }
-                        // calculate mean shift
-                        Vec3d mean = Vec3d(0.0, 0.0, 0.0);
-                        for (auto const &p : points) mean += p;
-                        mean /= (double)points.size();
-
-                        Vec3d ms = mean - c;
-                        if (sqrt(ms[0] * ms[0] + ms[1] * ms[1] + ms[2] * ms[2]) < 0.01)
-                        {
-                            converged[i] = true;
-                        }
-                        else
-                        {
-                            allconverged = false;
-                            c += mean - c;
-                        }
+                      }
+                      if(isInside)
+                      {
+                        inpoints.push_back(p);
+                      }
                     }
-                    ++i; // window index
+                    // calculate mean shift
+                    VEC3 mean; for (int i=0;i<D;++i) mean[i]=0;
+                    
+                    for (auto const &p : points) mean += p;
+                    mean /= (double)points.size();
+
+                    VEC3 ms = mean - c;
+                    double length2=0;
+                    for(int i=0;i<D;++i) length2+=ms[i]*ms[i];
+                    
+                    if (sqrt(length2) < shift_threshold)
+                    {
+                        converged[i] = true;
+                    }
+                    else
+                    {
+                        allconverged = false;
+                        c += mean - c;
+                    }
                 }
+                ++i; // window index
             }
         }
+    }
 
-    };
+};
 
 }
 
