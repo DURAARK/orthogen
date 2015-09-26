65c1c1174680628c6b3965ca76fad2e75d6a5a75
Ulrich Krispel <ulrich.krispel@vc.fraunhofer.at>
2015-09-26 15:42:11 +0200

    bygade test / manual align and removed some verbose msgs

--- test/bygade_manualshift.bat
+++ test/bygade_manualshift.bat
@@ -7,7 +7,7 @@ set ELEVATION=-1.0459598204934397 1.5694096034770006
 set bindir=I:\Projects\2014-04-DuraArk\dev\OrthoGen\MSVC2013_W64\Release
 set align=%bindir%\panoalign.exe
 
-REM JUST PERFORM SHIFT
+REM ====================== JUST PERFORM SHIFT
 
 rem set SHIFT=1800
 rem set IMG=Byg72_023
@@ -17,8 +17,37 @@ rem set SHIFT=3100
 rem set IMG=Byg72_004008
 rem %align% --imsrc %PANO%\%IMG%_Faro.jpg --imdst %PANO%\%IMG%_Manual.jpg --outname %PANO%\%IMG%_aligned.jpg --selrange %ELEVATION% --shift %SHIFT%
 
-set SHIFT=1450
-set IMG=Byg72_005009
+rem set SHIFT=1450
+rem set IMG=Byg72_005009
+rem %align% --imsrc %PANO%\%IMG%_Faro.jpg --imdst %PANO%\%IMG%_Manual.jpg --outname %PANO%\%IMG%_aligned.jpg --selrange %ELEVATION% --shift %SHIFT%
+
+rem set SHIFT=2950
+rem set IMG=Byg72_006012
+rem %align% --imsrc %PANO%\%IMG%_Faro.jpg --imdst %PANO%\%IMG%_Manual.jpg --outname %PANO%\%IMG%_aligned.jpg --selrange %ELEVATION% --shift %SHIFT%
+
+rem set SHIFT=4650
+rem set IMG=Byg72_008015
+rem %align% --imsrc %PANO%\%IMG%_Faro.jpg --imdst %PANO%\%IMG%_Manual.jpg --outname %PANO%\%IMG%_aligned.jpg --selrange %ELEVATION% --shift %SHIFT%
+
+rem set SHIFT=6250
+rem set IMG=Byg72_009016
+rem %align% --imsrc %PANO%\%IMG%_Faro.jpg --imdst %PANO%\%IMG%_Manual.jpg --outname %PANO%\%IMG%_aligned.jpg --selrange %ELEVATION% --shift %SHIFT%
+
+rem set SHIFT=13050
+rem set IMG=Byg72_009017
+rem %align% --imsrc %PANO%\%IMG%_Faro.jpg --imdst %PANO%\%IMG%_Manual.jpg --outname %PANO%\%IMG%_aligned.jpg --selrange %ELEVATION% --shift %SHIFT%
+
+rem set SHIFT=6450
+rem set IMG=Byg72_lotz027
+rem %align% --imsrc %PANO%\%IMG%_Faro.jpg --imdst %PANO%\%IMG%_Manual.jpg --outname %PANO%\%IMG%_aligned.jpg --selrange %ELEVATION% --shift %SHIFT%
+
+rem set SHIFT=9750
+rem set IMG=Byg72_lotz028
+rem %align% --imsrc %PANO%\%IMG%_Faro.jpg --imdst %PANO%\%IMG%_Manual.jpg --outname %PANO%\%IMG%_aligned.jpg --selrange %ELEVATION% --shift %SHIFT%
+
+set SHIFT=250
+set IMG=Byg72_024
 %align% --imsrc %PANO%\%IMG%_Faro.jpg --imdst %PANO%\%IMG%_Manual.jpg --outname %PANO%\%IMG%_aligned.jpg --selrange %ELEVATION% --shift %SHIFT%
+
 
 pause
--- test/run_bygade.bat
+++ test/run_bygade.bat
@@ -1,6 +1,8 @@
 set DATA=I:\Projects\2014-04-DuraArk\DataSets\D5.4_Byg72
 set ORTHOGEN=I:\Projects\2014-04-DuraArk\dev\OrthoGen\MSVC2013_W64\Release\orthogen.exe
 set ALIGN=I:\Projects\2014-04-DuraArk\dev\OrthoGen\MSVC2013_W64\Release\panoalign.exe
+
+if not exist Byg72 mkdir Byg72
 cd Byg72
 
 %ORTHOGEN% --e57metadata %DATA%\Byg72_e57metadata.json --walljson %DATA%\Byg72_wall.json --panopath %DATA%\pano --align %ALIGN% --exgeom 1 
